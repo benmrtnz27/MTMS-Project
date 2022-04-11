72fc5b294962871f30ac10c0c7136582df6338e5
ZyklonR <92772530+ZyklonR@users.noreply.github.com>
2022-04-11 16:27:47 -0700

    Update multi_threaded_merge_sort.cpp

--- multi_threaded_merge_sort.cpp
+++ multi_threaded_merge_sort.cpp
@@ -1,10 +1,51 @@
-#include <iostream>
+#include<iostream>
 #include <pthread.h>
-#include <time.h>
- using namespace std;
+
+#include<cmath>
+using namespace std;
+#define MAX 25
+
+
+int a[MAX];
+int part = 0;
+void divide(int a[],float n, float p)
+{
+    int s,i,j,h;
+    s=n/p;
+    j=0;
+    for(i=0;i<n;i++)
+    {
+    if(s==j){
+    cout<<endl;
+    j=0;
+    }
+    cout<<a[i]<<" ";
+    j++;
+    }
+}
 
 int main(){
-  
-  
-  return 0;
+
+
+
+srand((unsigned) time(NULL));
+  float p;
+    // Loop to get 25 random numbers
+    for(int i=0; i<=MAX; i++){
+        a[i] = rand() % 100;
+    // Print the random number
+        cout<< " " <<a[i];
+      
+
+    }
+    cout<<"\nEnter the value in which you want to divide:";
+    cin>>p;
+    divide(a, MAX, p);
+    /*
+    cout << "Sorted array: ";
+    for (int i = 0; i < MAX; i++)
+        cout << a[i] << " ";
+        */
+
+return 0;
 }
